2e0724a846b7c43fa17d55cc7ea132a978aa13b7
John Leider <john.j.leider@gmail.com>
2017-02-06 19:58:08 -0500

    updates in preparation for new release

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,58 +1,74 @@
 <template>
   <v-app top-navbar>
     <header>
-      <v-navbar>
-        <v-navbar-logo>Vuetify</v-navbar-logo>
-        <v-navbar-items>
-          <v-navbar-item :item="item"></v-navbar-item>
-        </v-navbar-items>
-      </v-navbar>
+      <v-toolbar>
+        <v-toolbar-logo>Vuetify</v-toolbar-logo>
+        <v-toolbar-items>
+          <v-toolbar-item>
+            {{ item.text }}
+          </v-toolbar-item>
+        </v-toolbar-items>
+      </v-toolbar>
     </header>
     <main>
       <v-content>
         <v-container fluid>
-          <v-card class="secondary">
-            <v-card-text>
-              <img src="public/v.png">
-            </v-card-text>
-          </v-card>
-          <p>Welcome to the Vuetify Webpack Template</p>
-          <h2>Important Links</h2>
-          <div class="intro">
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="http://vuetifyjs.com" target="_blank">Vuetify Documentation</a>
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="https://github.com/vuetifyjs/webpack-ssr" target="_blank">Vuetify SSR Webpack</a>
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="https://github.com/vuetifyjs/webpack" target="_blank">Vuetify Webpack</a>
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="https://github.com/vuetifyjs/webpack-simple" target="_blank">Vuetify Webpack Simple</a>
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="http://vuejs.org" target="_blank">Vue Documentation</a>
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title>
-                  <a href="https://github.com/vuejs/awesome-vue" target="_blank">Vue Awesome</a>
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
+          <div id="getting-started">
+            <v-card class="secondary">
+              <v-card-text class="text-xs-center">
+                <img src="public/v.png">
+              </v-card-text>
+            </v-card>
+            <p class="text-xs-center">Welcome to the Vuetify Webpack Template</p>
+            <h2 class="primary--text">Important Links</h2>
+            <div class="intro">
+              <v-list>
+                <v-list-item>
+                  <v-list-tile href="http://vuetifyjs.com" target="_blank">
+                    <v-list-tile-title>
+                      Vuetify Documentation
+                    </v-list-tile-title>
+                  </v-list-tile>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-tile href="https://github.com/vuetifyjs/webpack-ssr" target="_blank">
+                    <v-list-tile-title>
+                      Vuetify SSR Webpack
+                    </v-list-tile-title>
+                  </v-list-tile>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-tile href="https://github.com/vuetifyjs/webpack" target="_blank">
+                    <v-list-tile-title>
+                      Vuetify Webpack
+                    </v-list-tile-title>
+                  </v-list-tile>
+                </v-list-item>
+              </v-list>
+              <v-list>
+                <v-list-item>
+                  <v-list-tile href="https://github.com/vuetifyjs/webpack-simple" target="_blank">
+                    <v-list-tile-title>
+                      Vuetify Webpack Simple
+                    </v-list-tile-title>
+                  </v-list-tile>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-tile href="http://vuejs.org" target="_blank">
+                    <v-list-tile-title>
+                      Vue Documentation
+                    </v-list-tile-title>
+                  </v-list-tile>
+                </v-list-item>
+                <v-list-item>
+                    <v-list-tile href="https://github.com/vuejs/awesome-vue" target="_blank">
+                      <v-list-tile-title>
+                        Vue Awesome
+                      </v-list-tile-title>
+                    </v-list-tile>
+                </v-list-item>
+              </v-list>
+            </div>
           </div>
         </v-container>
       </v-content>
@@ -65,14 +81,9 @@
     data () {
       return {
         item: {
-          href: '#!',
           text: 'Get Started'
         }
       }
-    },
-
-    mounted () {
-      this.$vuetify.init()
     }
   }
 </script>
