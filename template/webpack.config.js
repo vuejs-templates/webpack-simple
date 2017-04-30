fdf8769af5b8a14aeafcbd4131982c5910711613
Žiga Vidic <zigomir@gmail.com>
2017-04-30 13:43:01 -0700

    Remove newline.

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -52,7 +52,6 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
-
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
