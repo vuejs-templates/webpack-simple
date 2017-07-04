205949ca4d34a582fe77102b34b317befcc1792a
John Leider <john.j.leider@gmail.com>
2017-07-04 11:09:49 -0400

    updated for 0.13

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app light>
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
@@ -7,22 +7,21 @@
       v-model="drawer"
     >
       <v-list>
-        <v-list-item
+        <v-list-tile 
           v-for="(item, i) in items"
           :key="i"
+          value="true"
         >
-          <v-list-tile value="true">
-            <v-list-tile-action>
-              <v-icon light v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
+    <v-toolbar fixed>
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-btn 
         icon
@@ -74,14 +73,12 @@
       v-model="rightDrawer"
     >
       <v-list>
-        <v-list-item>
-          <v-list-tile @click.native="right = !right">
-            <v-list-tile-action>
-              <v-icon light>compare_arrows</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-          </v-list-tile>
-        </v-list-item>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed">
