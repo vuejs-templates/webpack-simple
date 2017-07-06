c6778a4783e0b9a84d85e852e5c8f09bc1e4f3b4
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2017-07-06 01:11:35 +0100

    Revert "Remove extra character"
    This reverts commit f7e0de88667be9aef74a16acf04e62cfcc475ba7.

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+    <h1>\{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
