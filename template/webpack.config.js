3d93d83bb926621c3971e27db0520b20203ba0fb
Thorsten Lünborg <t.luenborg@googlemail.com>
2017-11-06 19:44:05 +0100

    add css loaders for files

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -11,16 +11,55 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },{{#sass}}
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSytanx'
+        ],
+      },
+      {{/sass}}
+      {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        use: 'vue-loader',
         options: {
           loaders: {
             {{#sass}}
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader'
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
             {{/sass}}
           }
           // other vue-loader options go here
@@ -28,12 +67,12 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        use: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]'
         }
