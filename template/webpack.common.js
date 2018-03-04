0765f79edb616fa585313336cee8f0f8234533c3
Rob Cresswell <robert.cresswell@outlook.com>
2018-03-04 11:18:27 +0000

    Update dependencies, including webpack v4
    This commit updates the templates dependencies, including webpack v4
    changes. Config that is automatically provided by webpacks new "mode"
    flag has been removed.

--- template/webpack.common.js
+++ template/webpack.common.js
@@ -77,35 +77,5 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+  }
 }
