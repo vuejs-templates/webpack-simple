073165704fd90f88a015907ff0a27336ff7a27e4
zhangdi <zhangdichn@gmail.com>
2018-03-06 14:39:32 +0800

    修改

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>\{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <lt-layout class="ltdemo-wrap" :width="layoutWidth">
+      <lt-header class="ltdemo-header" height="60px">
+        <div class="header-nav">
+          <span>Lantern</span>
+        </div>
+      </lt-header>
+      <lt-main class="ltdemo-main" height="100%">
+        <div class="main-wrap">
+          <lt-layout class="main-wrap-layout" :isRow="mainLayoutIsRow">
+            <lt-aside width="300px">
+              <lt-menu>
+                <lt-menu-item :href="'/'">index</lt-menu-item>
+                <lt-menu-item :href="'/test'">测试</lt-menu-item>
+              </lt-menu>
+            </lt-aside>
+            <lt-main height="100%">
+              <router-view></router-view>
+            </lt-main>
+          </lt-layout>
+        </div>
+      </lt-main>
+    </lt-layout>
   </div>
 </template>
 
@@ -24,37 +30,13 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      layoutWidth: '100%',
+      mainLayoutIsRow: true
     }
   }
 }
 </script>
-
-<style{{#sass}} lang="scss"{{/sass}}>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
 </style>
+
+
---
+++ template/src/route.config.js
@@ -0,0 +1,13 @@
+import index from './pages/index.vue';
+import test from './pages/test.vue';
+
+export default [
+  {
+    path: '/',
+    component: index
+  },
+  {
+    path: '/test',
+    component: test
+  }
+]
